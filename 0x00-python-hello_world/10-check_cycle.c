d74cf89dbc9912d2f30a2cae701384b650829a84
Nsikak-mbre <tm260715@gmail.com>
2024-03-15 17:29:46 +0100

    all

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#inlcude "list.h"
+
+/**
+ * check_cycle - checks for a cycle in a list
+ * @list: points to the first_node
+ * Return: 0 if no cycle, 1 there is cycle
+ */
+int check_cycle(listint_t *list)
+{
+	while(list ! = NULL)
+	{
+		if
+
