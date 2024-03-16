2ce852ee74923b32d043f65fb89e11ceac0f9e30
Nsikak-mbre <tm260715@gmail.com>
2024-03-16 16:20:59 +0100

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if isinstance(c, str) and len(c) == 1 and 97 <= ord(c) <= 122:
+    if len(c) != 1 or not c.isalpha():
+        return False
+    elif 'a' <= c <= 'z':
         return True
     else:
         return False
