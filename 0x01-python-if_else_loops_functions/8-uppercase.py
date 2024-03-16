71da062c155385554911047a270f54214c7622dc
Nsikak-mbre <tm260715@gmail.com>
2024-03-16 16:39:38 +0100

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,9 @@
 def uppercase(str):
-
+    uppercase_string = ''
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_chr = chr(ord(char) - 32)
+            uppercase_string += uppercase_chr
+        else:
+            uppercase_string += char
+    print(uppercase_string)
