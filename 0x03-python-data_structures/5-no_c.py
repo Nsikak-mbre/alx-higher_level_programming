0edf70024d1b18cb8ebef6db557177955cef0802
Nsikak-mbre <tm260715@gmail.com>
2024-03-18 10:31:24 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        new_string = ''
-    for char in range(len(my_string)):
-        if char not in ('c', 'C'):
-            new_string += char
-    print(new_string.format())
+    new_string = ''.join([char for char in my_string if char.lower() != 'c'])
+    return new_string
