ebf4d9e2b6f480b7e3e37a65ec720e7b6f42ed2f
Nsikak-mbre <tm260715@gmail.com>
2024-03-19 22:14:16 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,3 +3,6 @@ import sys
 print(len(sys.argv) - 1, 'arguments:')
 for i in range(1, len(sys.argv)):
     print('{}:{}'.format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    print('anything you like'
