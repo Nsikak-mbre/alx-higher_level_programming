4977d4d1d4edbc43623985437c840db6d00d0c77
Nsikak-mbre <tm260715@gmail.com>
2024-03-20 00:46:10 +0100

    3-infinte_add.py

---
+++ 0x02-python-import_modules/3-infinte_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    size = len(sys.argv)
+    result = 0
+    for i in range(1, size):
+        result += int(sys.argv[i])
+    print('{}'.format(result))
+
+
