61b8d0d62fca52b9dce118f35033ae7e65ea1222
Nsikak-mbre <tm260715@gmail.com>
2024-03-19 19:11:20 +0100

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
 a = 10
 b = 5
-addition = add(a, b)
-print('{} + {} = {}'.format(a, b, addition))
 
-subtract = sub(a, b)
-print('{} + {} = {}'.format(a, b, subtract))
+print('{} + {} = {}'.format(a, b, add(a, b)))
 
-divide = div(a, b)
-print('{} + {} = {}'.format(a, b, divide))
+print('{} + {} = {}'.format(a, b, sub(a, b)))
 
-multiply = mul(a, b)
-print('{} + {} = {}'.format(a, b, multiply))
+print('{} + {} = {}'.format(a, b, mul(a, b)))
+
+print('{} + {} = {}'.format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     add(a, b)
