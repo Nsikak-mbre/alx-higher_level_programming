0c7d72e1c9c8bc1af949eab1c67b254e64c5a87a
Nsikak-mbre <tm260715@gmail.com>
2024-03-24 16:24:33 +0100

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return abs(a) ^ abs(b)
+    if a < 0:
+        a = abs(a)
+    result = 1
+    if b < 0:
+        for _ in range(abs(b)):
+            result *= 1/a
+    else:
+        for _ in range(b):
+            result *= a
+    return result
