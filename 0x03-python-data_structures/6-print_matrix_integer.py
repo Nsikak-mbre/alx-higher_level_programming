a4c06d14a1fcb7a792da36180bd5d48473058981
Nsikak-mbre <tm260715@gmail.com>
2024-03-25 09:23:45 +0100

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        raw = ' '.join([f'{num:d}' for num in row])
+        raw =' '.join([f'{num:d}' for num in row])
         print('{}'.format(raw))
     return raw
