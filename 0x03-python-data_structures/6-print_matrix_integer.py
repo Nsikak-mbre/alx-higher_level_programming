83df6448a3502596d23c8d02e15e1753c5e56616
Nsikak-mbre <tm260715@gmail.com>
2024-03-25 09:21:05 +0100

    6-main.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         raw = ' '.join([f'{num:d}' for num in row])
         print('{}'.format(raw))
-        return raw
+    return raw
