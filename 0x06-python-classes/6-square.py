dff1e016488da4ecf12625d37e0c54999f0ea9db
Nsikak-mbre <tm260715@gmail.com>
2024-04-06 22:57:16 +0100

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,8 +48,8 @@ class Square:
             return
 
         if self.position[1] > 0:
-            for _ in range(self.position[1]):
+            for _ in range(self.__position[1]):
                 print()
 
         for _ in range(self.size):
-            print(' ' * self.position[0] + '#' * self.size)
+            print(' ' * self.__position[0] + '#' * self.__size)
