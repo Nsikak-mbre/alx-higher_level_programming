391c5fe578003bd247ef207e1d89d715c7c33147
Nsikak-mbre <tm260715@gmail.com>
2024-04-07 19:30:30 +0100

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    prints formatted value
+
+    Args:
+        (value): itme to be formatted
+
+    Returns:
+        True if rightly formatted
+    """
+    try:
+        formatted_value = '{:d}'.format(value)
+        print(formatted_value)
+        return True
+    except ValueError:
+        return False
