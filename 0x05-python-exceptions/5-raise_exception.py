a3f33977b57fbdf9d66875fed61a4f8278cbfee8
Nsikak-mbre <tm260715@gmail.com>
2024-04-08 17:42:51 +0100

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def raise_exception(text):
+    """
+
+    Function that raises a type exception
+
+    """
     try:
-        text = "The answer is: " +  4
+        text = "The answer is: " + 4
         return text
     except TypeError:
         print('Exception raised')
