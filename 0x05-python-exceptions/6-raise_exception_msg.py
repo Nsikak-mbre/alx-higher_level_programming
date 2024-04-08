acd4e0a7f0ed613d88debc020a514bf9dbdf19b8
Nsikak-mbre <tm260715@gmail.com>
2024-04-08 18:00:04 +0100

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+
+    raise a name exception
+
+    """
+    message = ' I hate C'
+    try:
+        print(x)
+    except NameError:
+        print('{}'.format(message))
