3a9658f774802e56ad6beb9deda4fc07d1be986c
Nsikak-mbre <tm260715@gmail.com>
2024-04-11 09:35:18 +0100

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """sums two Integers.
+
+    Parameters:
+    a (int): Integer provided by user
+    b (int): Integer by user or default
+
+    Returns:
+    Int: summation or an exception
+    """
+    if isinstance(a, (int, float)):
+        a = int(a)
+    else:
+        raise TypeError('a must be an integer')
+
+    if isinstance(b, (int, float)):
+        b = int(b)
+    else:
+        raise TypeError('b must be an integer')
+
+    return a + b
