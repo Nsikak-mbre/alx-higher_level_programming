d65300b36f215bb70e854b0e9d88e430cd17f61e
Nsikak-mbre <tm260715@gmail.com>
2024-04-11 23:46:14 +0100

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width (int): self explanatory
             height (int): salf explanatory
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
