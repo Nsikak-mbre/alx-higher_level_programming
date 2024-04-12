5281dd4de97a620902b4a9589e046029a6427e9d
Nsikak-mbre <tm260715@gmail.com>
2024-04-13 00:03:47 +0100

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,14 +12,14 @@ def matrix_divided(matrix, div):
     (list): new matrix where elements are divided by the divisor
     """
     if matrix is None or not matrix:
-        raise TypeError('matrix must be a matrix\
-                        (list of lists) of integers/floats')
+        raise TypeError('matrix must be a matrix (list of lists) of '
+                        'integers/floats')
 
     if not isinstance(matrix, list) or not all(isinstance(sub_list, list) and
        all(isinstance(elem, (int, float)) for elem in
        sub_list) for sub_list in matrix):
-        raise TypeError('matrix must be a matrix\
-                        (list of lists) of integers/floats')
+        raise TypeError('matrix must be a matrix (list of lists) of '
+                        'integers/floats')
 
     row_length = len(matrix[0])
     for sub_list in matrix:
