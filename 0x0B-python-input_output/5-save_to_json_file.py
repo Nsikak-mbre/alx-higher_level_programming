2576bebecd3fa31a46b9326b03c52fd6b9ff27c2
Nsikak-mbre <tm260715@gmail.com>
2024-04-25 11:34:03 +0100

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Returns Json representation of an object"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Accepts Object and file destination
+
+    Args:
+        my_obj(object): Data-type in Python
+
+    Returns:
+        Json tect representation of given Object
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file, indent=4)
