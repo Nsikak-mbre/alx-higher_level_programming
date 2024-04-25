98902bf49a13c76bd48d72a4a684ac7050e7ca77
Nsikak-mbre <tm260715@gmail.com>
2024-04-25 08:06:56 +0100

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-import json
 """convert object(string) to JSON"""
+import json
 
 
 def to_json_string(my_obj):
     """
-    Accepts an object
-
-    Args:
-        my_obj(any-type): string object to be serialized
-
     Returns:
         JSON string representation of given object
     """
