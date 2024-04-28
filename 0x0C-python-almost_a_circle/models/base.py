945384f5f15f2a24d4a5b41cce2c5d0c1a3f9e0d
Nsikak-mbre <tm260715@gmail.com>
2024-04-28 18:46:39 +0100

    0-main.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Base model"""
+
+
+class Base:
+    """Base from which other classes/methods gets derived
+
+    Private class Attribute:
+        __nb_object (int): Tracks number of Instantiation
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Base Constructor
+
+        Args:
+            id (int): Assigns id to derived object optionally
+        """
+        if id is not None:
+            self. id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
