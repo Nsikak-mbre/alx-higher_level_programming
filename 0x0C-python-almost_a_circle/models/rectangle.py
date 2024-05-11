e50a0e6bf700468a53af2e53ac1182ad24b5e857
Nsikak-mbre <tm260715@gmail.com>
2024-05-11 19:06:35 +0100

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,7 +84,7 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
-        """Custom string represnetation of class"""
+        """Custom string representation of rectangle class."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
