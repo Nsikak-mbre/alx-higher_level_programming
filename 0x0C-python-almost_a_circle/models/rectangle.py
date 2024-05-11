7f3afa17f2ccaa46377fe795726fa842273bc4f1
Nsikak-mbre <tm260715@gmail.com>
2024-05-11 19:23:37 +0100

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,8 +80,9 @@ class Rectangle(Base):
 
     def display(self):
         """prints out visual representation of Rectangle"""
+        print("\n" * self.y, end="")
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + '#' * self.width)
 
     def __str__(self):
         """Custom string representation of rectangle class."""
