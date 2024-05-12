c0c2ac5f100842921b95a6eced3113fdb521acbb
Nsikak-mbre <tm260715@gmail.com>
2024-05-12 13:24:09 +0100

    models/square.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base model"""
+import json
 
 
 class Base:
@@ -22,3 +23,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns JSON string representation."""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
