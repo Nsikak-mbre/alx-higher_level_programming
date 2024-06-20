ed890a83c51a250ff32142c9f8a657d6f9429262
Nsikak <tm260715@gmail.com>
2024-06-20 18:24:12 +0100

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,5 @@ CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    UNIQUE (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
