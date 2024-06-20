421dfbe3f440ce3069f2ac34463d726d4f4ed99e
Nsikak <tm260715@gmail.com>
2024-06-20 18:17:44 +0100

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
-    state_is INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa(id)
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
