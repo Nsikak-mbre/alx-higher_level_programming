9f608de032691adc32fd40d8165f3a90e51370b2
Nsikak-mbre <tm260715@gmail.com>
2024-06-25 13:22:55 +0100

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function factorial(number) {
-    if (isNaN(number) || number <= 1) {
-        return 1;
-    }
-    return number * (factorial(number - 1));
+function factorial (number) {
+  if (isNaN(number) || number <= 1) {
+    return 1;
+  }
+  return number * (factorial(number - 1));
 }
 const number = parseInt(process.argv[2], 10);
 console.log(factorial(number));
