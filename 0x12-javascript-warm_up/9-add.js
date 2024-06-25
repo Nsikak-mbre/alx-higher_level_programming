3f224ff788a7b451d6b199f984469c63427863db
Nsikak-mbre <tm260715@gmail.com>
2024-06-25 11:06:29 +0100

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function add(a, b) {
-    console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 const arg1 = parseInt(process.argv[2], 10);
 const arg2 = parseInt(process.argv[3], 10);
 if (isNaN(arg1) || isNaN(arg2)) {
-    console.log('NaN');
+  console.log('NaN');
 } else {
-    add(arg1, arg2);
+  add(arg1, arg2);
 }
