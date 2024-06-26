13b9480562938ed77db79dc314aaba5ab55a52cd
Nsikak-mbre <tm260715@gmail.com>
2024-06-26 23:23:06 +0100

    8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function reverseArr (arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    const temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr;
+}
+module.export = reverseArr;
