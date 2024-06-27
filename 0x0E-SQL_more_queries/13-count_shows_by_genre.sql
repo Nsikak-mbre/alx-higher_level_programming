83f825dd1006b64bc54b3ee3418f380555b72a3d
Nsikak-mbre <tm260715@gmail.com>
2024-06-27 12:25:13 +0100

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,17 @@
 -- import database dump
 -- lisitng genres and numbers of shows linked to each
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING number_of_shows > 0
-ORDER BY number_of_shows DESC;
+SELECT 
+    genres.name AS genre, 
+    COUNT(tvshows.id) AS number_of_shows
+FROM 
+    genres
+LEFT JOIN 
+    tvshows_genres ON genres.id = tvshows_genres.genre_id
+LEFT JOIN 
+    tvshows ON tvshows.id = tvshows_genres.tvshow_id
+GROUP BY 
+    genres.name
+HAVING 
+    number_of_shows > 0
+ORDER BY 
+    number_of_shows DESC;
