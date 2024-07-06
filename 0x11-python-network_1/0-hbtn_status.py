441012766f8558478f4b7811659b89ba61dae907
Nsikak-mbre <tm260715@gmail.com>
2024-07-06 19:55:14 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
 This module fetches https://alx-intranet.hbtn.io/status
-and displays the body of the response.
+and displays the body of the resp.
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        body = resp.read()
 
-    print('Body response:')
+    print('Body resp:')
     print('\t- type', type(body))
     print('\t- content:', body)
     print('\t- utf8 content:', body.decode('utf-8'))
