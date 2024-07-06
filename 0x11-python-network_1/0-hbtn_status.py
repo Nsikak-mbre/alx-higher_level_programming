cc498f97f8349cc9572ec64426c32e3a36734d64
Nsikak-mbre <tm260715@gmail.com>
2024-07-06 19:53:04 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,12 +6,12 @@ and displays the body of the response.
 
 
 if __name__ == '__main__':
-import urllib.request
+    import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
 
-print('Body response:')
-print('\t- type', type(body))
-print('\t- content:', body)
-print('\t- utf8 content:', body.decode('utf-8'))
+    print('Body response:')
+    print('\t- type', type(body))
+    print('\t- content:', body)
+    print('\t- utf8 content:', body.decode('utf-8'))
