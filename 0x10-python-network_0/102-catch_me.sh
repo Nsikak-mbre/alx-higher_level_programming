185d15ae0aadbc1376fa81ff581aa43dfdf232e3
Nsikak-mbre <tm260715@gmail.com>
2024-07-06 03:38:16 +0100

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  sends a request with, which causes a custom return message
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
