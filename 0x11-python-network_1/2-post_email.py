e207dc9db29b6d1be3a536069cb501cdf58c97fb
Nsikak-mbre <tm260715@gmail.com>
2024-07-07 18:55:35 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = 'email='.format(email)
+    data = 'email={}'.format(email)
     data = data.encode('utf-8')
 
     with urllib.request.urlopen(url, data) as response:
