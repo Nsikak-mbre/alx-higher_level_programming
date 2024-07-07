1d19823b340e8a5c36fdfffb4152a9602393fdc8
Nsikak-mbre <tm260715@gmail.com>
2024-07-07 18:32:02 +0100

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+sends POST request, display the body of the response
+"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = 'email='.format(email)
+    data = data.encode('utf-8')
+
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
