a904028f9c2e343ad9e8610490b1585d42af4934
Nsikak-mbre <tm260715@gmail.com>
2024-07-16 11:04:28 +0100

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,11 +5,11 @@ Displays values in table that matches specified arg, in a safe way
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     state_name = sys.argv[4]
     with MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306) as db:
-        with db.cursor as cursor:
+        with db.cursor() as cursor:
             query = ("SELECT * FROM states WHERE BINARY name = %s "
                      "ORDER BY id ASC")
             cursor.execute(query, (state_name,))
