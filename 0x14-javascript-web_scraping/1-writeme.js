f539210e44c3a465be0c16b4a66338323887e848
Nsikak-mbre <tm260715@gmail.com>
2024-08-02 14:47:34 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const process = require('process');
 const filePath = process.argv[2];
 const writeTo = process.argv[3];
 
 if (!filePath || !writeTo) { process.exit(1); }
 
-fs.writeFile(filePath, writeTo || '', 'utf-8', (err) => {
+fs.writeFile(filePath, writeTo, (err) => {
   if (err) {
     console.error(err);
-    process.exit(1);
   }
 });
