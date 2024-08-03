62cf81f182c03e2398782b8bbc442385b3fc4da8
Nsikak-mbre <tm260715@gmail.com>
2024-08-03 14:02:16 +0100

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
-const userId = '18';
-
-request(url, (error, response, body) => {
-  if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const films = data.results;
-    const userMovies = films.filter(film =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${userId}/`)
-    );
-    console.log(userMovies.length);
-  } else {
-    console.error(error);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
