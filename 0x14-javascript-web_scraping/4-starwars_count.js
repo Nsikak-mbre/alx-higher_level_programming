2a444682069df3e8ee3904b02c722cd7f896e96e
Nsikak-mbre <tm260715@gmail.com>
2024-08-03 13:36:31 +0100

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const userId = 18;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const films = data.results;
+    const userMovies = films.filter(film =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${userId}/`)
+    );
+    console.log(userMovies.length);
+  }
+});
