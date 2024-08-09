41437bceb0e0a94929b7706e0fd1ba808092c5e4
Nsikak-mbre <tm260715@gmail.com>
2024-08-09 05:38:48 +0100

    7-script.js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+    $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data){
+        const name = data.name;
+        $('#character').text('name')
+
+    })
+})
