63d2480017a51e0ab60e2b17ade463805fd80afa
Nsikak-mbre <tm260715@gmail.com>
2025-02-20 14:16:26 +0100

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -11,24 +11,24 @@ class Node:
 
     @property
     def data(self):
-        """Get data of node"""
+        """get data of node"""
         return self.__data
 
     @data.setter
     def data(self, value):
-        """Set data of node"""
+        """set data of node"""
         if not isinstance(value, int):
             raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
-        """Get next node"""
+        """get next node"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """Set next node"""
+        """points to next-node"""
         if not isinstance(value, Node) and value is not None:
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
@@ -41,17 +41,16 @@ class SinglyLinkedList:
         self.__head = None
 
     def __str__(self):
-        """String representation of singly linked list"""
+        """string representation of singly linked list"""
         string = ''
         current = self.__head
         while current:
             string += str(current.data) + '\n'
             current = current.next_node
-        return string.strip()
+        return string
 
     def sorted_insert(self, value):
         """Inserts a node in sorted order
-
         Args:
             value (int): value to insert
         """
